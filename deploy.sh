41f9eed35ff96c2752f273f815a78c4dddb10d0b
Danil Povarnitsyn <55835215+buffstyles@users.noreply.github.com>
2021-06-17 21:48:20 +0300

    Fixed deploy.sh syntax, added cloud-init file, append README

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
