8afdf5dbd706eefa021f6f401ecdb48df4eafa19
Danil Povarnitsyn <55835215+buffstyles@users.noreply.github.com>
2021-06-18 16:21:42 +0300

    Added files for packer-base homework

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install -y apt-transport-https
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update -y
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
