b6e8ba51137722164fdb4e7aa6e1909c7761c81e
Danil Povarnitsyn <55835215+buffstyles@users.noreply.github.com>
2021-06-18 22:53:16 +0300

    Fixing mistypes

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+IMAGE_FOLDER_ID=b1gprlskmuless8prlue
+
 yc compute instance create \
   --name reddit-full \
   --hostname reddit-full \
   --zone ru-central1-a \
   --memory=4 \
-  --create-boot-disk image-folder-id=b1gprlskmuless8prlue,image-family=reddit-full,size=10GB \
+  --create-boot-disk image-folder-id=$IMAGE_FOLDER_ID,image-family=reddit-full,size=10GB \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
   --metadata serial-port-enable=1 \
   --ssh-key ~/.ssh/appuser.pub
