e78364b100ea906ca93769e0402f5342a4399784
Danil Povarnitsyn <55835215+buffstyles@users.noreply.github.com>
2021-06-21 14:27:19 +0300

    Changed to dynamic block in lb_target_group

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -2,13 +2,12 @@ resource "yandex_lb_target_group" "reddit-target-group" {
   name      = "reddit-app-target-group"
   region_id = var.region_id
 
-  target {
-    subnet_id = var.subnet_id
-    address   = yandex_compute_instance.app[0].network_interface.0.ip_address
-  }
-  target {
-    subnet_id = var.subnet_id
-    address   = yandex_compute_instance.app[1].network_interface.0.ip_address
+  dynamic "target" {
+    for_each = yandex_compute_instance.app
+    content {
+      subnet_id = var.subnet_id
+      address   = target.value.network_interface.0.ip_address
+    }
   }
 }
 
