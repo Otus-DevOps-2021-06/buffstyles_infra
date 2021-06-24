ca15590734dc3c4b9225b0bf40e3b2c937fe57ba
Danil Povarnitsyn <55835215+buffstyles@users.noreply.github.com>
2021-06-24 20:40:13 +0300

    Added files for ansible-3 homework

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,15 +1,15 @@
-terraform {
-  backend "s3" {
-    endpoint = "storage.yandexcloud.net"
-    bucket   = "terraform-state-bucket-2"
-    region   = "ru-central1"
-    key      = "terraform.tfstate"
+# terraform {
+#   backend "s3" {
+#     endpoint = "storage.yandexcloud.net"
+#     bucket   = "terraform-state-bucket-2"
+#     region   = "ru-central1"
+#     key      = "terraform.tfstate"
 
-    # Данные Yandex Serverless Database для блокировки удаленного tfstate. Без них блокировки нет.
-    # dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1guu7b8p27pq5v031iu/etn01q6lcqgdm09hrass"
-    # dynamodb_table    = "tfstate-lock-table"
+#     # Данные Yandex Serverless Database для блокировки удаленного tfstate. Без них блокировки нет.
+#     # dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1guu7b8p27pq5v031iu/etn01q6lcqgdm09hrass"
+#     # dynamodb_table    = "tfstate-lock-table"
 
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
-}
+#     skip_region_validation      = true
+#     skip_credentials_validation = true
+#   }
+# }
